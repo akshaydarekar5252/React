10fc68093cc4c5115bf4c80ae6007fdbe4fbeec5
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-07-08 21:38:15 +0530

    react props

---
+++ src/ProductTab.jsx
@@ -0,0 +1,13 @@
+import Product from "./Product.jsx";
+
+function ProductTab() {
+  return (
+    <div>
+      <Product title="pen"/>
+      <Product title="laptop"/>
+      <Product title="phone"/>
+    </div>
+  );
+}
+
+export default ProductTab;
---
+++ src/Title.jsx
@@ -0,0 +1,5 @@
+function Title() {
+  return <h1>Welcome to my React App</h1>;
+}
+
+export default Title;
