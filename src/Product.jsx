b113b8dd2c8f60a23adce0bc34119573e3230e61
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-07-17 23:55:20 +0530

    conditionals

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,13 @@
 import "./Product.css";
 
-function Product({title}) {
+function Product({ title, price, features }) {
+    // const list = feature.map((feature) => <li>{feature}</li>);
+    let isDiscount = price>30000 ? "Discount 5%" : ""
     return (
         <div className="Product">
             <h3>{title}</h3>
-            <h5>Product Description</h5>
+            <h3>price: {price}</h3>
+            <h5>{isDiscount}</h5>
         </div>
     );
 }
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,11 +1,12 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
+  // let option1 = ["hello", "good morning", "hey hii"];
   return (
     <div>
-      <Product title="pen"/>
-      <Product title="laptop"/>
-      <Product title="phone"/>
+      <Product title="pen" price={30}/>
+      <Product title="laptop" price={30000} />
+      <Product title="phone" price={80000} />
     </div>
   );
 }
