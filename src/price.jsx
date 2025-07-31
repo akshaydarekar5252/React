be01b0c1cf85e8ee3711d646f65ad108511aab5b
Akshay Darekar <akshaydarekar5252@gmail.com>
2025-07-31 23:35:51 +0530

    dynamic component styling

--- src/price.jsx
+++ src/price.jsx
@@ -5,8 +5,14 @@ export default function Price({oldPrices, newPrices}) {
     let newStyles = {
         fontweight : "bold",
     }
+    let styles3 = {
+        backgroundColor : "#e0c367",
+        height :" 30px",
+        borderBottomLeftRadius : "15px",
+        borderBottomRightRadius : "15px",
+    }
     return (
-        <div>
+        <div style={styles3}>
             <span style={oldStyles}>{oldPrices}</span>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <span style={newStyles}>{newPrices}</span>
